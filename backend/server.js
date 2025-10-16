0d3648e30ff35a5035f6e9228b8f7271ca8fe9ef
Devika Sawant <devikasawant168@gmail.com>
2025-10-17 01:26:02 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 5000;
 const io = new Server(server); 
 
 // --- MIDDLEWARE ---
-app.use(cors());
+app.use(cors({ origin: 'https://main-sem-5-project.onrender.com', credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
